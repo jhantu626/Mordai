edfe84d05dcd9d6f37d0d6434b2b31a26b6efe8f
jhantu626 <jhantubala626@gmail.com>
2025-07-14 12:31:22 +0530

    dev: created the secondary header for other screens with back btn and title text

---
+++ src/components/Headers/SecondaryHeader.jsx
@@ -0,0 +1,35 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { fonts } from '../../utils/fonts';
+
+const SecondaryHeader = ({ title = 'Cart' }) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.backBtn}>
+        <Ionicons name="arrow-back" size={24} color="black" />
+      </TouchableOpacity>
+      <Text style={styles.titleText}>{title}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: 50,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  backBtn: {
+    position: 'absolute',
+    left: 0,
+  },
+  titleText: {
+    fontSize: 18,
+    fontFamily: fonts.semiBold,
+  },
+});
+
+export default SecondaryHeader;
--- src/components/index.js
+++ src/components/index.js
@@ -4,9 +4,17 @@ import SearchInput from './Inputs/SearchInput';
 
 // Headers
 import PrimaryHeader from './Headers/PrimaryHeader';
+import SecondaryHeader from './Headers/SecondaryHeader';
 
 // CARDS
 import BannerCard from './Cards/BannerCard';
 import ProductCard from './Cards/ProductCard';
 
-export { DefaultInput, PrimaryHeader, SearchInput, BannerCard, ProductCard };
+export {
+  DefaultInput,
+  PrimaryHeader,
+  SearchInput,
+  BannerCard,
+  ProductCard,
+  SecondaryHeader,
+};
