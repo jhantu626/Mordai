3a8be0c26e8b84bd05116d9c981ae10fab3bd8f2
jhantu626 <jhantubala626@gmail.com>
2025-07-16 16:23:02 +0530

    cahnges: some changes of cartContext

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -1,9 +1,35 @@
-import { Children, createContext, useContext, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 
 const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
   const [carts, setCarts] = useState([]);
+
+  useEffect(() => {
+    const loadCarts = async () => {
+      try {
+        const jsonValue = await AsyncStorage.getItem('carts');
+        if (jsonValue != null) {
+          setCarts(JSON.parse(jsonValue));
+        }
+      } catch (e) {
+        console.error('Failed to load carts from storage', e);
+      }
+    };
+    loadCarts();
+  }, []);
+
+  useEffect(() => {
+    const saveCarts = async () => {
+      try {
+        await AsyncStorage.setItem('carts', JSON.stringify(carts));
+      } catch (e) {
+        console.error('Failed to save carts to storage', e);
+      }
+    };
+    saveCarts();
+  }, [carts]);
 
   const addToCart = ({ cart }) => {
     setCarts(prevCarts => {
@@ -12,38 +38,47 @@ const CartProvider = ({ children }) => {
       );
 
       if (index !== -1) {
-        // Increase quantity by 1
         const updatedCarts = [...prevCarts];
         updatedCarts[index].quantity += 1;
         return updatedCarts;
       } else {
-        // Add new item with quantity 1
         return [...prevCarts, { ...cart, quantity: 1 }];
       }
     });
   };
 
   const removeFromCart = ({ item }) => {
-    setCarts(prevCarts =>
-      prevCarts.filter(
-        cartItem => !(cartItem.id === item.id && cartItem.size === item.size),
-      ),
-    );
+    setCarts(prevCarts => {
+      if (item.quantity === 1) {
+        return prevCarts.filter(
+          cartItem => !(cartItem.id === item.id && cartItem.size === item.size),
+        );
+      } else {
+        return prevCarts.map(cartItem => {
+          if (cartItem.id === item.id && cartItem.size === item.size) {
+            return { ...cartItem, quantity: cartItem.quantity - 1 };
+          }
+          return cartItem;
+        });
+      }
+    });
   };
 
   return (
     <CartContext.Provider
-      value={{ carts, addToCart, removeFromCart, numOfCarts: carts.length }}
+      value={{
+        carts,
+        addToCart,
+        removeFromCart,
+        numOfCarts: carts.reduce((sum, item) => sum + item.quantity, 0),
+      }}
     >
       {children}
     </CartContext.Provider>
   );
 };
 
-export default CartProvider;
-
-const useCartContext = () => {
-  return useContext(CartContext);
-};
+const useCartContext = () => useContext(CartContext);
 
 export { useCartContext };
+export default CartProvider;
