845dec84e5cb9054b24cf46c53398f6ff542b80a
jhantu626 <jhantubala626@gmail.com>
2025-07-17 16:44:11 +0530

    added more images

--- src/screens/AppScreens/Home/Home.jsx
+++ src/screens/AppScreens/Home/Home.jsx
@@ -75,9 +75,57 @@ const product = [
       { label: '2 pcs', price: 55.0, originalPrice: 70.0 },
     ],
     category: 'Fruits',
-    image: require('./../../../../assets/images/product1.png'),
+    image: require('./../../../../assets/images/product5.png'),
+  },
+  {
+    id: 6,
+    name: 'Carrot',
+    sizes: [
+      { label: '250g', price: 12.0, originalPrice: 15.0 },
+      { label: '500g', price: 22.0, originalPrice: 28.0 },
+    ],
+    discount: '7% Off',
+    category: 'Vegetables',
+    image: require('./../../../../assets/images/product6.png'),
+  },
+  {
+    id: 7,
+    name: 'Pineapple Juice',
+    sizes: [
+      { label: '500ml', price: 35.0, originalPrice: 40.0 },
+      { label: '1L', price: 60.0, originalPrice: 65.0 },
+    ],
+    discount: '8% Off',
+    category: 'Juices',
+    image: require('./../../../../assets/images/product7.png'),
+  },
+  {
+    id: 8,
+    name: 'Banana',
+    sizes: [{ label: '1 dozen', price: 40.0, originalPrice: 45.0 }],
+    category: 'Fruits',
+    image: require('./../../../../assets/images/product8.png'),
+  },
+  {
+    id: 9,
+    name: 'Tomato',
+    sizes: [
+      { label: '250g', price: 10.0, originalPrice: 12.0 },
+      { label: '500g', price: 18.0, originalPrice: 22.0 },
+    ],
+    discount: '5% Off',
+    category: 'Vegetables',
+    image: require('./../../../../assets/images/product9.png'),
+  },
+  {
+    id: 10,
+    name: 'Watermelon Slice',
+    sizes: [{ label: '1 slice', price: 25.0, originalPrice: 30.0 }],
+    category: 'Fruits',
+    image: require('./../../../../assets/images/product10.png'),
   },
 ];
+
 
 const category = [
   {
