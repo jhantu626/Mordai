285ee846b6984110698aa6f549afaa19cbc60efa
jhantu626 <jhantubala626@gmail.com>
2025-07-28 23:41:40 +0530

    dev: added the secondary header

--- src/screens/AppScreens/Account/Accounts.jsx
+++ src/screens/AppScreens/Account/Accounts.jsx
@@ -1,11 +1,12 @@
 import { ScrollView, StyleSheet } from 'react-native'
 import React from 'react'
 import Layout from '../../Layout/Layout'
-import { ProfileCard } from '../../../components'
+import { ProfileCard, SecondaryHeader } from '../../../components'
 
 const Accounts = () => {
   return (
     <Layout>
+      <SecondaryHeader title='Profile'/>
       <ScrollView style={{flex: 1}}>
         <ProfileCard/>
       </ScrollView>
