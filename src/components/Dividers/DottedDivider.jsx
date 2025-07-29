ac07f1e38605399f39597bc5359cc5c1cc4d20b8
jhantu626 <jhantubala626@gmail.com>
2025-07-29 12:38:00 +0530

    dev: dynamic width of border

--- src/components/Dividers/DottedDivider.jsx
+++ src/components/Dividers/DottedDivider.jsx
@@ -1,13 +1,14 @@
 import { StyleSheet, View } from 'react-native';
 import React from 'react';
 
-const DottedDivider = () => {
-  return <View style={styles.divider} />;
+const DottedDivider = ({borderWidth=2}) => {
+  return <View style={[styles.divider,{
+    borderBottomWidth: borderWidth
+  }]} />;
 };
 
 const styles = StyleSheet.create({
   divider: {
-    borderBottomWidth: 2,
     borderBottomColor: '#D9D9D9',
     borderStyle: 'dashed',
     width: '100%',
