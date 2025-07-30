503304e29a06dbf17f988a4f5e9e3a05eadc06fd
jhantu626 <jhantubala626@gmail.com>
2025-07-30 15:54:22 +0530

    added few validations

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,13 +1,53 @@
 function validateIndianPhoneNumber(phoneNumber) {
-    const hasPlus = phoneNumber.startsWith('+');
-    const cleanNumber = phoneNumber.replace(/[\s\-]/g, '');
-    
-    const numberForValidation = cleanNumber.replace(/^\+/, '');
-    
-    const mobilePattern = /^91[6789]\d{9}$/;
-    
-    return hasPlus && mobilePattern.test(numberForValidation);
+  const hasPlus = phoneNumber.startsWith('+');
+  const cleanNumber = phoneNumber.replace(/[\s\-]/g, '');
+
+  const numberForValidation = cleanNumber.replace(/^\+/, '');
+
+  const mobilePattern = /^91[6789]\d{9}$/;
+
+  return hasPlus && mobilePattern.test(numberForValidation);
 }
 
+function validatePersonName(name) {
+  const trimmedName = name.trim();
 
-export {validateIndianPhoneNumber}
+  if (!trimmedName) {
+    return false;
+  }
+
+  if (trimmedName.length < 2 || trimmedName.length > 50) {
+    return false;
+  }
+
+  const namePattern = /^[\p{L}]+(?:[\s\-'.]+[\p{L}]+)*$/u;
+
+  if (!namePattern.test(trimmedName)) {
+    return false;
+  }
+
+  if (/\s{2,}/.test(trimmedName)) {
+    return false;
+  }
+
+  if (/\d/.test(trimmedName)) {
+    return false;
+  }
+
+  return true;
+}
+
+function validateIndianPincode(pincode) {
+  const cleanPincode = pincode.toString().replace(/[\s\-]/g, '');
+
+  if (!/^\d{6}$/.test(cleanPincode)) {
+    return false;
+  }
+  const firstDigit = parseInt(cleanPincode[0]);
+  if (firstDigit === 0) {
+    return false;
+  }
+  return firstDigit >= 1 && firstDigit <= 9;
+}
+
+export { validateIndianPhoneNumber, validatePersonName,validateIndianPincode };
