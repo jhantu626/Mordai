bb2587005281777867bfb8ec4a0762acacf83f19
jhantu626 <jhantubala626@gmail.com>
2025-08-05 17:56:11 +0530

    delete address with bottomsheet

--- src/screens/AppScreens/Account/Address.jsx
+++ src/screens/AppScreens/Account/Address.jsx
@@ -7,99 +7,174 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import React, { useState } from 'react';
+import React, { useMemo, useRef, useState } from 'react';
 import Layout from '../../Layout/Layout';
 import { DottedDivider, SecondaryHeader } from '../../../components';
 import Entypo from 'react-native-vector-icons/Entypo';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import Octicons from 'react-native-vector-icons/Octicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import { fonts } from '../../../utils/fonts';
 import { colors } from '../../../utils/colors';
 import { useNavigation } from '@react-navigation/native';
 import { useAuth } from '../../../contexts/AuthContext';
 import { useAddress } from '../../../contexts/AddressContext';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import BottomSheet, {
+  BottomSheetBackdrop,
+  BottomSheetView,
+} from '@gorhom/bottom-sheet';
 
 const Address = () => {
   const [hasSavedAddress, setHasSavedAddress] = useState(true);
   const navigation = useNavigation();
 
-  const { address } = useAddress();
+  // REF VARIABLES
+  const bottomSheetRef = useRef(null);
+  const snapPoints = useMemo(() => ['25%'], []);
 
-  console.log(JSON.stringify(address));
+  const { address, removeAddress } = useAddress();
+
+  // STATE VARIABLES
+  const [selectedAddress, setSelectedAddress] = useState(address[0]);
+
+  const renderBackdrop = useMemo(
+    () => props =>
+      (
+        <BottomSheetBackdrop
+          {...props}
+          disappearsOnIndex={-1}
+          appearsOnIndex={0}
+          opacity={0.5}
+        />
+      ),
+    [],
+  );
 
   return (
-    <Layout>
-      <SecondaryHeader title={'Address'} />
-      <ScrollView style={{ flex: 1 }} contentContainerStyle={styles.container}>
-        <TouchableOpacity
-          style={styles.addCard}
-          onPress={() => {
-            navigation.navigate('AddAddress');
-          }}
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Layout>
+        <SecondaryHeader title={'Address'} />
+        <ScrollView
+          style={{ flex: 1 }}
+          contentContainerStyle={styles.container}
         >
-          <View style={styles.leftContainer}>
-            <AntDesign name="plus" size={20} color={colors.pinkColor} />
-            <Text style={styles.addText}>Add New Address</Text>
-          </View>
-          <Entypo name="chevron-small-right" size={24} color="#000000" />
-        </TouchableOpacity>
-        <Text style={styles.savedText}>Saved Addresses</Text>
-        {!hasSavedAddress && (
-          <View style={styles.notFOundContainer}>
-            <Image
-              style={styles.image}
-              source={require('./../../../../assets/images/address.png')}
-            />
-            <Text style={styles.notFoundText}>No Saved Address</Text>
-          </View>
-        )}
-        <View style={styles.addreddContainer}>
-          <FlatList
-            data={address}
-            keyExtractor={(_, index) => index + '-addresses'}
-            renderItem={({ item, index }) => (
-              <View>
-                <View style={styles.addressCard}>
-                  <View
-                    style={[styles.leftContainer, { gap: 10, width: '80%' }]}
-                  >
-                    <Octicons name="location" size={24} color={'#00000090'} />
-                    <View>
-                      <View style={{ flexDirection: 'row', gap: 5 }}>
-                        <Text style={styles.titleText}>{item.labe}</Text>
-                        {index === 0 && <Text style={styles.selectedText}>Selected</Text>}
-                      </View>
-                      <Text numberOfLines={1} style={styles.addressText}>
-                        {item.house}, {item.building}, {item.area}, {item.pincode},{' '}
-                      </Text>
-                    </View>
-                  </View>
-                  <TouchableOpacity>
-                    <Entypo
-                      name="dots-three-vertical"
-                      size={20}
-                      color="#00000090"
-                    />
-                  </TouchableOpacity>
-                </View>
-                {index !== 2 && <DottedDivider borderWidth={1} />}
-              </View>
-            )}
-            ListEmptyComponent={() => {
-              return (
-                <View style={styles.notFOundContainer}>
-                  <Image
-                    style={styles.image}
-                    source={require('./../../../../assets/images/address.png')}
-                  />
-                  <Text style={styles.notFoundText}>No Saved Address</Text>
-                </View>
-              );
+          <TouchableOpacity
+            style={styles.addCard}
+            onPress={() => {
+              navigation.navigate('AddAddress');
             }}
-          />
-        </View>
-      </ScrollView>
-    </Layout>
+          >
+            <View style={styles.leftContainer}>
+              <AntDesign name="plus" size={20} color={colors.pinkColor} />
+              <Text style={styles.addText}>Add New Address</Text>
+            </View>
+            <Entypo name="chevron-small-right" size={24} color="#000000" />
+          </TouchableOpacity>
+          <Text style={styles.savedText}>Saved Addresses</Text>
+          {!hasSavedAddress && (
+            <View style={styles.notFOundContainer}>
+              <Image
+                style={styles.image}
+                source={require('./../../../../assets/images/address.png')}
+              />
+              <Text style={styles.notFoundText}>No Saved Address</Text>
+            </View>
+          )}
+          <View style={styles.addreddContainer}>
+            <FlatList
+              data={address}
+              keyExtractor={(_, index) => index + '-addresses'}
+              renderItem={({ item, index }) => (
+                <View>
+                  <View style={styles.addressCard}>
+                    <View
+                      style={[styles.leftContainer, { gap: 10, width: '80%' }]}
+                    >
+                      <Octicons name="location" size={24} color={'#00000090'} />
+                      <View>
+                        <View style={{ flexDirection: 'row', gap: 5 }}>
+                          <Text style={styles.titleText}>{item.labe}</Text>
+                          {index === 0 && (
+                            <Text style={styles.selectedText}>Selected</Text>
+                          )}
+                        </View>
+                        <Text numberOfLines={1} style={styles.addressText}>
+                          {item.house}, {item.building}, {item.area},{' '}
+                          {item.pincode},{' '}
+                        </Text>
+                      </View>
+                    </View>
+                    <TouchableOpacity
+                      onPress={() => {
+                        bottomSheetRef.current.expand();
+                        setSelectedAddress(item);
+                      }}
+                    >
+                      <Entypo
+                        name="dots-three-vertical"
+                        size={20}
+                        color="#00000090"
+                      />
+                    </TouchableOpacity>
+                  </View>
+                  {index !== 2 && <DottedDivider borderWidth={1} />}
+                </View>
+              )}
+              ListEmptyComponent={() => {
+                return (
+                  <View style={styles.notFOundContainer}>
+                    <Image
+                      style={styles.image}
+                      source={require('./../../../../assets/images/address.png')}
+                    />
+                    <Text style={styles.notFoundText}>No Saved Address</Text>
+                  </View>
+                );
+              }}
+            />
+          </View>
+        </ScrollView>
+      </Layout>
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={snapPoints}
+        index={-1}
+        backdropComponent={renderBackdrop}
+        animationConfigs={{
+          duration: 200,
+        }}
+        enablePanDownToClose
+        handleComponent={null}
+      >
+        <BottomSheetView style={styles.bottomSheetContainer}>
+          <View>
+            <Text style={styles.bottomsheetTitleText}>
+              {selectedAddress.labe}
+            </Text>
+            <Text style={styles.bottomsheetAddressText}>
+              {selectedAddress.house}, {selectedAddress.building},{' '}
+              {selectedAddress.area}, {selectedAddress.pincode}
+            </Text>
+          </View>
+          <TouchableOpacity style={styles.buttonContainer}>
+            <AntDesign name="edit" size={36} color="#000000" />
+            <Text style={styles.buttonText}>Edit</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.buttonContainer}
+            onPress={async () => {
+              console.log('delete', selectedAddress);
+              await removeAddress({ add: selectedAddress });
+              bottomSheetRef.current.close();
+            }}
+          >
+            <MaterialIcons name="delete-outline" size={36} color="#FF0000" />
+            <Text style={styles.buttonText}>Delete</Text>
+          </TouchableOpacity>
+        </BottomSheetView>
+      </BottomSheet>
+    </GestureHandlerRootView>
   );
 };
 
@@ -184,6 +259,32 @@ const styles = StyleSheet.create({
   addressText: {
     fontSize: 12,
     fontFamily: fonts.light,
+    color: '#000000',
+  },
+  bottomSheetContainer: {
+    paddingHorizontal: 10,
+    paddingVertical: 15,
+    gap: 15,
+  },
+  bottomsheetTitleText: {
+    fontSize: 15,
+    fontFamily: fonts.semiBold,
+    color: '#000000',
+  },
+  bottomsheetAddressText: {
+    fontSize: 13,
+    fontFamily: fonts.regular,
+    color: '#000000',
+    width: '90%',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontFamily: fonts.medium,
     color: '#000000',
   },
 });
