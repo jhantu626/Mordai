cd02bc85c68521ab746fde681d71f2a4e95e2848
jhantu626 <jhantubala626@gmail.com>
2025-08-11 13:33:14 +0530

    completed new category

--- src/components/Cards/ExploreNewCategory.jsx
+++ src/components/Cards/ExploreNewCategory.jsx
@@ -8,15 +8,39 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { get } from 'react-native/Libraries/TurboModule/TurboModuleRegistry';
 import { fonts } from '../../utils/fonts';
 import { colors } from '../../utils/colors';
 import LinearGradient from 'react-native-linear-gradient';
+import axios from 'axios';
+import { categoryService } from '../../services/CategoryService';
+import CategoryCardShimmer from '../Shimmers/CategoryCardShimmer';
+import CategoryShimmer from '../Shimmers/CategoryShimmer';
 
 const { width } = Dimensions.get('window');
 
 const ExploreNewCategory = () => {
+  const [categories, setCategories] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchCategories = async () => {
+    try {
+      setIsLoading(true);
+      const data = await categoryService.getCategories();
+      console.log('categoris', data);
+      setCategories(data.success ? data?.category : []);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.headerText}>
@@ -28,44 +52,44 @@ const ExploreNewCategory = () => {
       </View>
       <Text style={styles.nextTitle}>Grab New Arrivals Fast</Text>
       <View style={styles.categoryContainer}>
-        <ScrollView
+        <FlatList
           horizontal
           showsHorizontalScrollIndicator={false}
           contentContainerStyle={{ gap: 10, marginTop: 10 }}
-        >
-          {[
-            'Fruits',
-            'Vegetables',
-            'Dairy',
-            'Bread & Bakery',
-            'Meat & Fish',
-          ].map((item, index) => (
-            <View key={index + 'category-item'}>
-              <LinearGradient
-                colors={['#d9cbe6', '#d1b8d9']}
-                //   colors={['#90EE90', '#73C973']}
-                style={styles.imageCOntainer}
-              >
-                <TouchableOpacity
-                  style={{
-                    width: '100%',
-                    height: '100%',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                  }}
+          data={isLoading ? [1, 2, 3, 4] : categories}
+          keyExtractor={(_, index) => 'category-item-' + index}
+          scrollEnabled={!isLoading}
+          renderItem={({ item, index }) =>
+            isLoading ? (
+              <CategoryCardShimmer />
+            ) : (
+              <View key={index + 'category-item'}>
+                <LinearGradient
+                  colors={['#d9cbe6', '#d1b8d9']}
+                  //   colors={['#90EE90', '#73C973']}
+                  style={styles.imageCOntainer}
                 >
-                  <Image
-                    style={styles.categoryImage}
-                    source={require('./../../../assets/images/cat1.png')}
-                  />
-                </TouchableOpacity>
-              </LinearGradient>
-              <Text style={styles.text} numberOfLines={2}>
-                {item}
-              </Text>
-            </View>
-          ))}
-        </ScrollView>
+                  <TouchableOpacity
+                    style={{
+                      width: '100%',
+                      height: '100%',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}
+                  >
+                    <Image
+                      style={styles.categoryImage}
+                      source={{ uri: item.image }}
+                    />
+                  </TouchableOpacity>
+                </LinearGradient>
+                <Text style={styles.text} numberOfLines={2}>
+                  {item.name}
+                </Text>
+              </View>
+            )
+          }
+        />
       </View>
     </View>
   );
@@ -108,6 +132,6 @@ const styles = StyleSheet.create({
     fontFamily: fonts.semiBold,
     textAlign: 'center',
     marginTop: 5,
-    width: width/3-50
+    width: width / 3 - 50,
   },
 });
